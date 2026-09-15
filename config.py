763e23316c3c5ee1b107c1c74e2d4914186c7735
sameer55044 <mosameer24460@gmail.com>
2026-09-15 22:10:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sameer55044/sameerop,
+    "https://github.com/sameer55044/sameerop"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
